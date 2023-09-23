d41d4092ade24580441a0ee092eda71b38271290
LaBoa99 <72893451+LaBoa99@users.noreply.github.com>
2023-09-22 22:21:57 -0600

    Clase 2 Gotas FINA

--- clase_2_gotas/main.py
+++ clase_2_gotas/main.py
@@ -1,14 +1,22 @@
 import sys
+import os
 import random
 import pygame
 from pygame.locals import *
- 
+
+"""
+Toma la ubicacion del archivo para ubicar la carpeta assets
+"""
+scrip_dir = os.path.dirname(__file__)
+
 """
 Aqui se define la clase gotas que hereda de la clase Sprite de Pygame
 Sprite le hereda diferentes funciones a nuesta clase gota
 la palabra self funciona para referirse a si mismo y se encuentra en todas
 las funciones dentro de Gota
 """
+
+
 class Gota(pygame.sprite.Sprite):
     """
     la funcion __init__ siempre se llama cuando se crea una gota
@@ -28,21 +36,26 @@ class Gota(pygame.sprite.Sprite):
 
     si te fijas el * hace que se puedan poner N numeros de variables o parametros
     """
-    def _init_(self,pos, *groups) -> None:
-       super().__init__(*groups)
-       self.image = pygame.image.load('assets/gota.png').convert_alpha()
-       self.rect = self.image.get_rect(topleft=pos)
-    
+
+    def __init__(self, pos, *groups) -> None:
+        super().__init__(*groups)
+        self.image = pygame.image.load(
+            os.path.join(scrip_dir, "assets/gota.png")
+        ).convert_alpha()
+        self.rect = self.image.get_rect(topleft=pos)
+
     """
     La funcion update es heredada de Sprite pero se puede sobrescribir como lo hacemos aqui
     aqui se actualiza la posicion del rectangulo en su eje Y
     Delta time es la diferencia de tiempo entre fotogramas
     kill() elimina la instancia de gota 
     """
+
     def update(self, dt):
         self.rect.y += 9.8 * dt
         if self.rect.y > 480:
             self.kill()
+
 
 """
 Al igual que gota la clase Cubeta hereda de Sprite teniendo los mismos metodos que Gota 
@@ -54,16 +67,21 @@ una instancia es cualquier objeto creado  es decir:
 cubeta = Cubeta() esto es una instancia
 lista = [] Esto es una instancia de la clase List
 """
+
+
 class Cubeta(pygame.sprite.Sprite):
-    def _init_(self,pos, *groups) -> None:
-       super().__init__(*groups)
-       self.image = pygame.image.load('assets/cubeta.png').convert_alpha()
-       self.rect = self.image.get_rect(topleft=pos)
+    def __init__(self, pos, *groups) -> None:
+        super().__init__(*groups)
+        self.image = pygame.image.load(
+            os.path.join(scrip_dir, "assets/cubeta.png")
+        ).convert_alpha()
+        self.rect = self.image.get_rect(topleft=pos)
+
 
 # Se inicia el motor
 pygame.init()
- 
- # Se define el tiempo o la tasa de refresco
+
+# Se define el tiempo o la tasa de refresco
 fps = 60
 clock = pygame.time.Clock()
 
@@ -80,15 +98,19 @@ pantalla = pygame.display.set_mode((ancho, altura))
 curren_time = pygame.time.get_ticks()
 previous_time = curren_time
 
+
 # Game loop.
 def agregar_gota():
-    x_pos = random.randint(0, ancho - 64)  # Ancho de la ventana menos el ancho de la gota
+    x_pos = random.randint(
+        0, ancho - 64
+    )  # Ancho de la ventana menos el ancho de la gota
     Gota((x_pos, -64), gotas)
 
+
 # se crea un evento mayor a USEREVENT para evitar colisiones
-# Recuerda que la funcion init de cubeta pide pos, y grupos 
+# Recuerda que la funcion init de cubeta pide pos, y grupos
 pygame.time.set_timer(USEREVENT + 1, 2000)
-cubeta = Cubeta((ancho // 2 - 32 , altura - 100), jugador)
+cubeta = Cubeta((ancho // 2 - 32, altura - 100), jugador)
 while True:
     # se llena la pantalla de un color
     pantalla.fill((138, 239, 255))
@@ -100,14 +122,14 @@ while True:
             sys.exit()
         elif event.type == USEREVENT + 1:
             agregar_gota()
-            
+
     # Capturar las pulsaciones de teclas para mover la cubeta
     keys = pygame.key.get_pressed()
     if keys[K_LEFT]:
         cubeta.rect.x -= 9.8
     if keys[K_RIGHT]:
         cubeta.rect.x += 9.8
-    
+
     # Esta funcion de aqui MAX retorna el numero mayor entre 0  y el numero minimo entre
     # la esquina superior izquierda y la resta de ancho - la anchura de la cubeta
     # permitiendonos saber si la cubeta arrebasa o no los limites de anchura
@@ -120,13 +142,13 @@ while True:
     # Update.
     deltatime = (curren_time - previous_time) / 1000.0  # Tiempo en segundos
     previous_time = deltatime
-    
+
     gotas.update(deltatime)
     jugador.update(deltatime)
     # Draw.
     gotas.draw(pantalla)
     jugador.draw(pantalla)
-    
+
     # Se limpia la pantalla
     pygame.display.flip()
     # Se ajusta nuestros FPS a lo esperado
